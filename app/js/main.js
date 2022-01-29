3476ee0d1f8dce7a3089fae0d14f1c48ed933736
Stepan PC <Stefan.irk96@mail.ru>
2022-01-29 20:38:17 +0800

    add header and mobil menu and section screen

---
+++ app/js/main.js
@@ -0,0 +1,35 @@
+window.onscroll = function showHeader() {
+  let header = document.querySelector('.header');
+  if (window.pageYOffset > 200) {
+    header.classList.add('header__fixed');
+  } else {
+    header.classList.remove('header__fixed');
+  }
+}
+
+
+$(function () {
+  $('.menu__btn').on('click', (function () {
+    if (!$('.menu').hasClass('active')) {
+      $('.menu').addClass('active');
+      $('.header').addClass('active');
+      $('html, body').css('overflow', 'hidden');
+    } else {
+      $('.menu').removeClass('active');
+      $('.header').removeClass('active');
+      $('html, body').css('overflow', 'auto');
+    }
+  }));
+  $('.screen__menu-btn').on('click', (function () {
+    if (!$('.menu').hasClass('active')) {
+      $('.menu').addClass('active');
+      $('.header').addClass('active');
+      $('html, body').css('overflow', 'hidden');
+    } else {
+      $('.menu').removeClass('active');
+      $('.header').removeClass('active');
+      $('html, body').css('overflow', 'auto');
+    }
+  }));
+
+})
