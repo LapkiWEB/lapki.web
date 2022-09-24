edef419d247ee3d3fdd5e086aca88b60dd603bcb
Stepan PC <Stefan.irk96@mail.ru>
2022-09-24 21:46:43 +0800

    add str page-policy

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,9 @@
+
+  // if (window.pageYOffset > 200) {
+
 window.onscroll = function showHeader() {
   let header = document.querySelector('.header');
-  if (window.pageYOffset > 200) {
+  if (window.pageYOffset > 150) {
     header.classList.add('header__fixed');
   } else {
     header.classList.remove('header__fixed');
